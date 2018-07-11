11f5f8b0ee982772a5b170e97760a0ad3b77266f
iMoD1998 <imod1998@fs0ciety.sh>
2018-07-11 15:33:59 +0100

    Changed converting method to regex.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 var fs = require('fs');
-fs.readFile('./gxt2.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  //Do your processing, MD5, send a satellite to the moon, etc.
-  var output = 'Citizen.CreateThread(function() \n'
-  data.toString().split("\n").forEach(function(line, index, arr) {
-    //if (index === arr.length - 1 && line === "") { return; }
-    line = line.trim();
-    if (line != '' && line != '\n') {
-      var arr = line.split(' = ')
-      var nline = ' AddTextEntry("' + arr[0] + '", "' + arr[1] + '")'
-      console.log('Converting ', line, ' -> ', nline);
-      output += '\tAddTextEntry("' + arr[0] + '", "' + arr[1] + '") \n'
-    }
-  });
-  output += 'end)'
-  fs.writeFile ("./names.lua", output, function(err) {
-    if (err) throw err;
-    console.log('complete \n');
 
-    require('readline')
-        .createInterface(process.stdin, process.stdout)
-        .question("Press [Enter] to exit...", function(){
-            process.exit();
+var regex = /(0x[A-Z-0-9]+)\s*=\s*(.*)/gim;
+
+fs.readFile('./gxt2.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+
+    var output = 'Citizen.CreateThread(function() \n';
+
+    var matches = data.match(regex);
+
+    if(matches == null)
+    {
+        console.error("Could not find any GXT entries...");
+        return;
+    }
+
+    matches.forEach(function(match) {
+        var newline = match.replace(regex, "AddTextEntry('$1', '$2')");
+        console.log('Converting ', match, ' -> ', newline);
+        output += '\t' + newline + "\n";
     });
-  });
+    
+    output += 'end)'
+
+    console.log(output);
+
+    fs.writeFile ("./names.lua", output, function(err) {
+        if (err) throw err;
+
+        console.log('Successfully converted ', matches.length, ' entries');
+    });
 });
