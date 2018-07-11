8c63fb9d44cc2ee551290118cf83300c9ed6f5f5
Starystars67 <starystars67@gmail.com>
2018-07-11 12:27:01 +0100

    v 1.1

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ fs.readFile('./gxt2.txt', 'utf8', function (err, data) {
     //if (index === arr.length - 1 && line === "") { return; }
     line = line.trim();
     if (line != '' && line != '\n') {
-      console.log('FILE -> ', line);
       var arr = line.split(' = ')
-      output += '	AddTextEntry("' + arr[0] + '", "' + arr[1] + '") \n'
+      var nline = ' AddTextEntry("' + arr[0] + '", "' + arr[1] + '")'
+      console.log('Converting ', line, ' -> ', nline);
+      output += '\tAddTextEntry("' + arr[0] + '", "' + arr[1] + '") \n'
     }
   });
   output += 'end)'
   fs.writeFile ("./names.lua", output, function(err) {
     if (err) throw err;
-    console.log('complete');
+    console.log('complete \n');
+
+    require('readline')
+        .createInterface(process.stdin, process.stdout)
+        .question("Press [Enter] to exit...", function(){
+            process.exit();
+    });
   });
 });
